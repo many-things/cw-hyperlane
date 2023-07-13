d714712ef2137c0a16c500935c9143fec6ba7d67
byeongsu-hong <hong@byeongsu.dev>
2023-07-13 20:24:03 +0900

    wip

--- contracts/igp-gas-oracle/src/contract.rs
+++ contracts/igp-gas-oracle/src/contract.rs
@@ -31,7 +31,7 @@ pub fn instantiate(
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
-    env: Env,
+    _env: Env,
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
