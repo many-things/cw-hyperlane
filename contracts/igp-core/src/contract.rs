dfe5aad63f7421048e44023323761a6276689660
byeongsu-hong <hong@byeongsu.dev>
2023-07-13 19:25:54 +0900

    feat: scaffolding

---
+++ contracts/igp-core/src/contract.rs
@@ -0,0 +1,45 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, QueryResponse, Response};
+
+use hpl_interface::igp_core;
+
+use crate::error::ContractError;
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: igp_core::InstantiateMsg,
+) -> Result<Response, ContractError> {
+    Ok(Response::default())
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: igp_core::ExecuteMsg,
+) -> Result<Response, ContractError> {
+    Ok(Response::default())
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(
+    deps: Deps,
+    env: Env,
+    msg: igp_core::QueryMsg,
+) -> Result<QueryResponse, ContractError> {
+    Ok(Binary::default())
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn migrate(
+    deps: DepsMut,
+    env: Env,
+    msg: igp_core::MigrateMsg,
+) -> Result<Response, ContractError> {
+    Ok(Response::default())
+}
