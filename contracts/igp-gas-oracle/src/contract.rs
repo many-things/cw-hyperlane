85bf3da7973629d3241f75e2fae727f7b95a3fea
byeongsu-hong <hong@byeongsu.dev>
2023-07-14 19:48:26 +0900

    apply

--- contracts/igp-gas-oracle/src/contract.rs
+++ contracts/igp-gas-oracle/src/contract.rs
@@ -1,8 +1,6 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{
-    to_binary, Deps, DepsMut, Env, Event, MessageInfo, QueryResponse, Response, StdError,
-};
+use cosmwasm_std::{to_binary, Deps, DepsMut, Env, Event, MessageInfo, QueryResponse, Response};
 
 use hpl_interface::igp_gas_oracle::{
     ConfigResponse, ExecuteMsg, GetExchangeRateAndGasPriceResponse, InstantiateMsg, MigrateMsg,
@@ -33,7 +31,7 @@ pub fn instantiate(
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
-    _env: Env,
+    env: Env,
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
