9d931cc644f0630fa9c5134fa17ea97d0c9843c9
byeongsu-hong <hong@byeongsu.dev>
2023-08-14 21:42:23 +0900

    fix: token-native schema

--- contracts/token-native/src/bin/schema.rs
+++ contracts/token-native/src/bin/schema.rs
@@ -2,7 +2,8 @@ use std::{env::current_dir, fs::remove_dir_all};
 
 use cosmwasm_schema::write_api;
 
-use hpl_interface::va::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use hpl_interface::token_native::{ExecuteMsg, QueryMsg};
+use hpl_token_native::msg::{InstantiateMsg, MigrateMsg};
 
 fn main() {
     write_api! {
