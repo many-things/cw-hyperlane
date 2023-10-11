37bcfbb819bc329b08b547bea2cd97739f5180fc
byeongsu-hong <hong@byeongsu.dev>
2023-10-11 23:01:31 +0900

    apply to routing-hook

--- packages/interface/src/hook/routing.rs
+++ packages/interface/src/hook/routing.rs
@@ -1,12 +1,13 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::HexBinary;
+use cosmwasm_std::Addr;
 
 use crate::{
-    hook::{HookConfig, OwnerResponse, PauseInfoResponse},
-    ownable::OwnableMsg,
+    ownable::{OwnableMsg, OwnableQueryMsg},
+    pausable::{PausableMsg, PausableQueryMsg},
+    router::{RouterMsg, RouterQuery},
 };
 
-use super::{PostDispatchMsg, QuoteDispatchResponse};
+use super::{HookQueryMsg, PostDispatchMsg};
 
 #[cw_serde]
 pub struct InstantiateMsg {
@@ -16,37 +17,42 @@ pub struct InstantiateMsg {
 
 #[cw_serde]
 pub enum ExecuteMsg {
-    Ownership(OwnableMsg),
-    Pause {},
-    Unpause {},
-    UpdateMailbox {
-        mailbox: String,
-    },
-    SetHook {
-        destination: u32,
-        hook: String,
-    },
-    SetHooks {
-        hooks: Vec<HookConfig>,
-    },
-    PostDispatch {
-        metadata: HexBinary,
-        message: HexBinary,
-    },
-}
+    // overrides
+    Ownable(OwnableMsg),
+    Pausable(PausableMsg),
+    PostDispatch(PostDispatchMsg),
 
-#[cw_serde]
-pub enum MigrateMsg {}
+    // routing
+    UpdateMailbox { mailbox: String },
+    Router(RouterMsg<Addr>),
+}
 
 #[cw_serde]
 #[derive(QueryResponses)]
+#[serde(untagged)]
+#[query_responses(nested)]
 pub enum QueryMsg {
-    #[returns(QuoteDispatchResponse)]
-    QuoteDispatch(PostDispatchMsg),
+    // overrides
+    Pausable(PausableQueryMsg),
+    Ownable(OwnableQueryMsg),
+    Hook(HookQueryMsg),
 
-    #[returns(PauseInfoResponse)]
-    PauseInfo {},
-
-    #[returns(OwnerResponse)]
-    Owner {},
+    // routing
+    Routing(RoutingQueryMsg),
+    Router(RouterQuery<Addr>),
 }
+
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum RoutingQueryMsg {
+    #[returns(ConfigResponse)]
+    Config {},
+}
+
+#[cw_serde]
+pub struct ConfigResponse {
+    pub mailbox: String,
+}
+
+#[cw_serde]
+pub struct MigrateMsg {}
