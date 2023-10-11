a05cbf5a357fad8da8b837c9112d3d6cda746d47
byeongsu-hong <hong@byeongsu.dev>
2023-10-11 13:41:23 +0900

    test bech32 with various hrp

--- packages/interface/src/types/bech32.rs
+++ packages/interface/src/types/bech32.rs
@@ -46,40 +46,60 @@ pub fn bech32_encode(hrp: &str, raw_addr: &[u8]) -> StdResult<Addr> {
 
 #[cfg(test)]
 mod tests {
+    use rstest::rstest;
+
     use crate::types::bech32_to_h256;
 
     use super::{bech32_decode, bech32_encode};
 
-    #[test]
-    fn test_decode_encode_full() {
-        let hrp = "osmo";
-        let src_addr = "osmo1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqkk0zfx";
-
+    #[rstest]
+    #[case(
+        "osmo",
+        "osmo1466nf3zuxpya8q9emxukd7vftaf6h4psr0a07srl5zw74zh84yjqkk0zfx"
+    )]
+    #[case("osmo", "osmo14n3a65fnqz9jve85l23al6m3pjugf0atvrfqh5")]
+    #[case(
+        "neutron",
+        "neutron1gajw625kz8el4ayk8fwpy7r6ew0m7zrg9jdd6grg85fle39shuxqezuz2c"
+    )]
+    #[case("neutron", "neutron1xle8l3h0wkcp6tsxmkc6n4vqyfkhwnukevwwsk")]
+    fn test_decode_encode(#[case] hrp: &str, #[case] src_addr: &str) {
         let dec_addr = bech32_to_h256(src_addr).unwrap();
         let enc_addr = bech32_encode(hrp, &dec_addr).unwrap();
 
         assert_eq!(src_addr, enc_addr.as_str());
     }
 
-    #[test]
-    fn test_decode_encode_full_padded() {
-        let hrp = "osmo";
-        let src_addr = "osmo14n3a65fnqz9jve85l23al6m3pjugf0atvrfqh5";
-
-        let dec_addr = bech32_to_h256(src_addr).unwrap();
-        let enc_addr = bech32_encode(hrp, &dec_addr).unwrap();
-
-        assert_eq!(src_addr, enc_addr.as_str());
-    }
-
-    #[test]
-    fn test_decode_encode_acc() {
-        let hrp = "osmo";
-        let src_addr = "osmo14n3a65fnqz9jve85l23al6m3pjugf0atvrfqh5";
-
+    #[rstest]
+    #[case("osmo", "osmo1d6a3j0kkpc8eac0j8h6ypyevfz8hd3qnuqjrc5")]
+    #[case("neutron", "neutron1d6a3j0kkpc8eac0j8h6ypyevfz8hd3qnsyg35p")]
+    fn test_decode_encode_acc(#[case] hrp: &str, #[case] src_addr: &str) {
         let dec_addr = bech32_decode(src_addr).unwrap();
         let enc_addr = bech32_encode(hrp, &dec_addr).unwrap();
 
         assert_eq!(src_addr, enc_addr.as_str());
+    }
+
+    #[rstest]
+    #[case(&[
+        "cosmos1d6a3j0kkpc8eac0j8h6ypyevfz8hd3qn5mpnwx",
+        "axelar1d6a3j0kkpc8eac0j8h6ypyevfz8hd3qns4hm98",
+        "osmo1d6a3j0kkpc8eac0j8h6ypyevfz8hd3qnuqjrc5",
+        "neutron1d6a3j0kkpc8eac0j8h6ypyevfz8hd3qnsyg35p"
+    ])]
+    fn test_acc_cross(#[case] addrs: &[&str]) {
+        for (i, test_target_addr) in addrs.iter().enumerate() {
+            let mut test_addr_group = addrs.to_vec();
+            test_addr_group.remove(i);
+
+            let test_target_addr_bin = bech32_decode(test_target_addr).unwrap();
+
+            for test_addr in test_addr_group {
+                let hrp = test_addr.split('1').collect::<Vec<_>>()[0];
+
+                let encoded = bech32_encode(hrp, &test_target_addr_bin).unwrap();
+                assert_eq!(test_addr, encoded.as_str());
+            }
+        }
     }
 }
