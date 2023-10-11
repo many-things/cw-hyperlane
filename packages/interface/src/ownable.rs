eef9466e2eaf9e8e12da3103fbef8dc11770b003
byeongsu-hong <hong@byeongsu.dev>
2023-10-11 22:03:44 +0900

    ownable

--- packages/interface/src/ownable.rs
+++ packages/interface/src/ownable.rs
@@ -1,8 +1,29 @@
-use cosmwasm_schema::cw_serde;
+use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Addr;
 
 #[cw_serde]
 pub enum OwnableMsg {
     InitOwnershipTransfer { next_owner: String },
     RevokeOwnershipTransfer {},
     ClaimOwnership {},
+}
+
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum OwnableQueryMsg {
+    #[returns(OwnerResponse)]
+    GetOwner {},
+
+    #[returns(PendingOwnerResponse)]
+    GetPendingOwner {},
+}
+
+#[cw_serde]
+pub struct OwnerResponse {
+    pub owner: Addr,
+}
+
+#[cw_serde]
+pub struct PendingOwnerResponse {
+    pub pending_owner: Option<Addr>,
 }
