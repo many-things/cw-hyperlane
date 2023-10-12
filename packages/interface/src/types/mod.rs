e0b40bc0a157cbf29ca195cdd56c26b069000244
byeongsu-hong <hong@byeongsu.dev>
2023-10-12 12:30:41 +0900

    fix: export message directly

--- packages/interface/src/types/mod.rs
+++ packages/interface/src/types/mod.rs
@@ -1,9 +1,10 @@
 mod bech32;
 mod crypto;
+mod message;
 
 pub mod merkle;
-pub mod message;
 pub mod metadata;
 
 pub use crate::types::bech32::{bech32_decode, bech32_encode, bech32_to_h256};
 pub use crate::types::crypto::keccak256_hash;
+pub use crate::types::message::Message;
