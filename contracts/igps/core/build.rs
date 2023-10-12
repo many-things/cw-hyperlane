20b8387dac944b416a62371ad8387f19fd48cdde
byeongsu-hong <hong@byeongsu.dev>
2023-10-12 13:59:57 +0900

    feat(igp): schema builder

--- contracts/igps/core/build.rs
+++ contracts/igps/core/build.rs
@@ -1,13 +1,14 @@
 use std::{env::current_dir, fs::remove_dir_all};
 
 use cosmwasm_schema::write_api;
+use cosmwasm_std::Empty;
 
-use hpl_interface::igp_core::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use hpl_interface::igp::core::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
-        migrate: MigrateMsg,
+        migrate: Empty,
         execute: ExecuteMsg,
         query: QueryMsg,
     }
@@ -15,4 +16,6 @@ fn main() {
     let mut raw_dir = current_dir().unwrap();
     raw_dir.push("schema");
     remove_dir_all(raw_dir.join("raw")).unwrap();
+
+    println!("cargo:rerun-if-changed=build.rs");
 }
