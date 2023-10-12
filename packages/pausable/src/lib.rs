4fb1092d1a15272ff66f5d0e388112038f49aa78
byeongsu-hong <hong@byeongsu.dev>
2023-10-12 10:49:06 +0900

    test: pausable

--- packages/pausable/src/lib.rs
+++ packages/pausable/src/lib.rs
@@ -1,15 +1,28 @@
+#[cfg(test)]
+mod test;
+
 use cosmwasm_std::{
-    ensure_eq, to_binary, Addr, CustomQuery, Deps, DepsMut, Env, Event, MessageInfo, QueryResponse,
-    Response, StdError, StdResult, Storage,
+    ensure, ensure_eq, to_binary, Addr, CustomQuery, Deps, DepsMut, Env, Event, MessageInfo,
+    QueryResponse, Response, StdError, StdResult, Storage,
 };
 use cw_storage_plus::Item;
 use hpl_interface::pausable::{PausableMsg, PausableQueryMsg, PauseInfoResponse};
 
-pub const PAUSE_KEY: &str = "pause";
-pub const PAUSE: Item<bool> = Item::new(PAUSE_KEY);
+const PAUSE_KEY: &str = "pause";
+const PAUSE: Item<bool> = Item::new(PAUSE_KEY);
+
+fn event_to_resp(event: Event) -> Response {
+    Response::new().add_event(event)
+}
 
 fn new_event(name: &str) -> Event {
     Event::new(format!("hpl_pausable::{}", name))
+}
+
+pub fn initialize(storage: &mut dyn Storage, paused: &bool) -> StdResult<()> {
+    PAUSE.save(storage, paused)?;
+
+    Ok(())
 }
 
 pub fn handle<C: CustomQuery>(
@@ -25,19 +38,27 @@ pub fn handle<C: CustomQuery>(
     );
 
     match msg {
-        PausableMsg::Pause {} => Ok(Response::new().add_event(pause(deps.storage, &info.sender)?)),
-        PausableMsg::Release {} => {
-            Ok(Response::new().add_event(release(deps.storage, &info.sender)?))
-        }
+        PausableMsg::Pause {} => Ok(event_to_resp(pause(deps.storage, &info.sender)?)),
+        PausableMsg::Release {} => Ok(event_to_resp(release(deps.storage, &info.sender)?)),
     }
 }
 
-fn pause(storage: &mut dyn Storage, sender: &Addr) -> StdResult<Event> {
+pub fn pause(storage: &mut dyn Storage, sender: &Addr) -> StdResult<Event> {
+    ensure!(
+        !PAUSE.load(storage)?,
+        StdError::generic_err("already paused")
+    );
+
     PAUSE.save(storage, &true)?;
     Ok(new_event("pause").add_attribute("sender", sender))
 }
 
-fn release(storage: &mut dyn Storage, sender: &Addr) -> StdResult<Event> {
+pub fn release(storage: &mut dyn Storage, sender: &Addr) -> StdResult<Event> {
+    ensure!(
+        PAUSE.load(storage)?,
+        StdError::generic_err("already released")
+    );
+
     PAUSE.save(storage, &false)?;
     Ok(new_event("release").add_attribute("sender", sender))
 }
