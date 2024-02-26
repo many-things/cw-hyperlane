0d7f6cdbf6fa19a6ac1c266b60d56a9d470175ce
byeongsu-hong <hong@byeongsu.dev>
2024-02-26 20:34:48 +0900

    feat(script): add test-dispatch

--- script/commands/contract.ts
+++ script/commands/contract.ts
@@ -1,9 +1,45 @@
 import { Command } from "commander";
 import { contractNames } from "../shared/constants";
+import { executeContract } from "../shared/contract";
+import { CONTAINER, Dependencies } from "../shared/ioc";
+import { getNetwork } from "../shared/config";
+import { addPad } from "../shared/utils";
 
-export const contractCmd = new Command("contract");
+export const contractCmd = new Command("contract").configureHelp({
+  showGlobalOptions: true,
+});
 
 contractCmd.command("list").action(() => {
   console.log("Available contracts:".green);
   contractNames.forEach((v) => console.log("-", v));
 });
+
+contractCmd
+  .command("test-dispatch")
+  .argument("dest-domian")
+  .argument("recipient-addr")
+  .argument("msg-body")
+  .action(async (destDomain, recipientAddr, msgBody, _, cmd) => {
+    const { ctx, client } = CONTAINER.get(Dependencies);
+
+    const mailbox = ctx.deployments.core?.mailbox!;
+
+    const res = await executeContract(
+      client,
+      mailbox,
+      {
+        dispatch: {
+          dest_domain: parseInt(destDomain),
+          recipient_addr: addPad(
+            recipientAddr.startsWith("0x")
+              ? recipientAddr.slice(2)
+              : recipientAddr
+          ),
+          msg_body: Buffer.from(msgBody, "utf-8").toString("hex"),
+        },
+      },
+      [{ amount: "500", denom: "ustrd" }]
+    );
+
+    console.log(res.hash);
+  });
