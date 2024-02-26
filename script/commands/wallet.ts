29196f30d2aa9d3e2c284df1ae4897af423f2e51
byeongsu-hong <hong@byeongsu.dev>
2024-02-27 01:21:07 +0900

    feat: wallet generator

--- script/commands/wallet.ts
+++ script/commands/wallet.ts
@@ -1,14 +1,36 @@
 import {
   DirectSecp256k1HdWallet,
   DirectSecp256k1Wallet,
+  makeCosmoshubPath,
 } from "@cosmjs/proto-signing";
 import { Command } from "commander";
 
 import { getNetwork } from "../shared/config";
+import { getKeyPair } from "../shared/crypto";
 
 const walletCmd = new Command("wallet")
   .description("Wallet commands")
   .configureHelp({ showGlobalOptions: true });
+
+walletCmd
+  .command("new")
+  .description("Create a new wallet")
+  .action(async (_, cmd) => {
+    const opts = cmd.optsWithGlobals();
+    const network = getNetwork(opts.networkId);
+
+    const prefix = { prefix: network.hrp };
+    const wallet = await DirectSecp256k1HdWallet.generate(12, prefix);
+
+    const [account] = await wallet.getAccounts();
+    const { privkey } = await getKeyPair(wallet.mnemonic, makeCosmoshubPath(0));
+
+    console.log({
+      address: account.address,
+      mnemonic: wallet.mnemonic,
+      privateKey: Buffer.from(privkey).toString("hex"),
+    });
+  });
 
 walletCmd
   .command("address")
