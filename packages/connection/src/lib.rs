7573576c97fe9ee9a91c3e4557ff5a32bfbcee40
Kunal Arora <55632507+aroralanuk@users.noreply.github.com>
2025-01-03 16:42:07 +0530

    feat(connection): add option to unset ISM and hook (#143)
    * add none to set_ism and set_hook
    
    * Update packages/connection/src/lib.rs
    
    Co-authored-by: Yorke Rhodes <email@yorke.dev>
    
    * Update packages/connection/src/lib.rs
    
    Co-authored-by: Yorke Rhodes <email@yorke.dev>
    
    * rm unset return test req
    
    * rm unset ism req
    
    ---------
    
    Co-authored-by: Yorke Rhodes <email@yorke.dev>

--- packages/connection/src/lib.rs
+++ packages/connection/src/lib.rs
@@ -49,22 +49,44 @@ pub fn handle<C: CustomQuery>(
             ))
         }
         SetIsm { ism } => {
-            let ism_addr = deps.api.addr_validate(&ism)?;
+            match ism {
+                Some(ism) => {
+                    let ism_addr = deps.api.addr_validate(&ism)?;
 
-            ISM.save(deps.storage, &ism_addr)?;
+                    ISM.save(deps.storage, &ism_addr)?;
 
-            Ok(event_to_resp(
-                new_event("set_ism").add_attribute("ism", ism),
-            ))
+                    Ok(event_to_resp(
+                        new_event("set_ism").add_attribute("ism", ism),
+                    ))
+                }
+                None => {
+                    ISM.remove(deps.storage);
+
+                    Ok(event_to_resp(
+                        new_event("unset_ism")
+                    ))
+                }
+            }
         }
         SetHook { hook } => {
-            let hook_addr = deps.api.addr_validate(&hook)?;
+            match hook {
+                Some(hook) => {
+                    let hook_addr = deps.api.addr_validate(&hook)?;
 
-            HOOK.save(deps.storage, &hook_addr)?;
+                    HOOK.save(deps.storage, &hook_addr)?;
 
-            Ok(event_to_resp(
-                new_event("set_hook").add_attribute("hook", hook),
-            ))
+                    Ok(event_to_resp(
+                        new_event("set_hook").add_attribute("hook", hook),
+                    ))
+            }
+                None => {
+                    HOOK.remove(deps.storage);
+
+                    Ok(event_to_resp(
+                        new_event("unset_hook")
+                    ))
+                }
+            }
         }
     }
 }
@@ -97,4 +119,89 @@ pub fn get_ism(storage: &dyn Storage) -> StdResult<Option<Addr>> {
 
 pub fn get_hook(storage: &dyn Storage) -> StdResult<Option<Addr>> {
     HOOK.may_load(storage)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::{
+        testing::{mock_dependencies, mock_env, mock_info},
+        Addr,
+    };
+    use ibcx_test_utils::addr;
+    use rstest::rstest;
+
+    const OWNER: &str = "owner";
+    const NOT_OWNER: &str = "not_owner";
+
+    #[rstest]
+    #[case(addr(OWNER), addr("new_ism_address"))]
+    #[should_panic(expected = "unauthorized")]
+    #[case(addr(NOT_OWNER), addr("new_ism_address"))]
+    fn test_set_and_unset_ism(#[case] sender: Addr, #[case] new_ism_addr: Addr) {
+        let mut deps = mock_dependencies();
+
+        hpl_ownable::initialize(deps.as_mut().storage, &Addr::unchecked(OWNER)).unwrap();
+
+        assert!(get_ism(&deps.storage).unwrap().is_none());
+
+        let msg = ConnectionMsg::SetIsm {
+            ism: Some(new_ism_addr.to_string()),
+        };
+        let info = mock_info(sender.as_str(), &[]);
+
+        let res = handle(deps.as_mut(), mock_env(), info, msg).unwrap();
+        assert_eq!(
+            res,
+            event_to_resp(
+                new_event("set_ism").add_attribute("ism", new_ism_addr.to_string())
+            )
+        );
+
+        let ism = get_ism(&deps.storage).unwrap().unwrap();
+        assert_eq!(ism, new_ism_addr);
+
+        let unset_msg = ConnectionMsg::SetIsm { ism: None };
+        let unset_info = mock_info(sender.as_str(), &[]);
+
+        handle(deps.as_mut(), mock_env(), unset_info, unset_msg).unwrap();
+
+        let ism = get_ism(&deps.storage).unwrap();
+        assert!(ism.is_none());
+    }
+
+    #[rstest]
+    #[case(addr(OWNER), addr("new_hook_address"))]
+    #[should_panic(expected = "unauthorized")]
+    #[case(addr(NOT_OWNER), addr("new_hook_address"))]
+    fn test_set_and_unset_hook(#[case] sender: Addr, #[case] new_hook_addr: Addr) {
+        let mut deps = mock_dependencies();
+
+        hpl_ownable::initialize(deps.as_mut().storage, &Addr::unchecked(OWNER)).unwrap();
+        assert!(get_hook(&deps.storage).unwrap().is_none());
+
+        let msg = ConnectionMsg::SetHook {
+            hook: Some(new_hook_addr.to_string()),
+        };
+        let info = mock_info(sender.as_str(), &[]);
+
+        let res = handle(deps.as_mut(), mock_env(), info, msg).unwrap();
+        assert_eq!(
+            res,
+            event_to_resp(
+                new_event("set_hook").add_attribute("hook", new_hook_addr.to_string())
+            )
+        );
+
+        let hook = get_hook(&deps.storage).unwrap().unwrap();
+        assert_eq!(hook, new_hook_addr);
+
+        let unset_msg = ConnectionMsg::SetHook { hook: None };
+        let unset_info = mock_info(sender.as_str(), &[]);
+
+        handle(deps.as_mut(), mock_env(), unset_info, unset_msg).unwrap();
+        
+        let hook = get_hook(&deps.storage).unwrap();
+        assert!(hook.is_none());
+    }
 }
